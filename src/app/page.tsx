b13f5b0fd37f3e779c5388879dbf78ce140d88f5
Pedro Sanches <sanchespedro40@gmail.com>
2023-08-31 15:54:40 -0300

    js-finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,62 @@ export default function Home() {
     const[values, setValues] = useState<string[][]>([[],[],[]])
     const[lastValue, setLastValue] = useState("")
 
+    const[winner, setWinner] = useState("")
+
   function setvalue(row:number, col:number) {
-    if(values[row][col]) return
+    if(winner != "" || values[row][col]) return 
     values[row][col] = lastValue == "X" ? "O" : "X"
     setLastValue(values[row][col])
     setValues(values)
+    if (verifyWinner()) {
+      setWinner(values[row][col])
+    }
   }
+
+  function verifyWinner():boolean {
+    
+
+    if(values[0][0] != undefined && values[0][0] == values[0][1] != undefined && values[0][1] && values[0][1] === values[0][2] && values[0][2] != undefined) {
+      console.log("1")
+      return true}
+
+    if(values[1][0] != undefined && values[1][0] == values[1][1]!= undefined && values[1][1] && values[1][1] == values[1][2] && values[1][2] != undefined) {
+      console.log("2")
+      return true}
+
+    if(values[2][0] != undefined && values[2][0] == values[2][1]!= undefined && values[2][1] && values[2][1] == values[2][2] && values[2][2] != undefined) {
+      console.log("3")
+      return true}
+
+    if(values[0][0] != undefined && values[0][0] == values[1][0]!= undefined && values[1][0] && values[1][0] == values[2][0] && values[2][0] != undefined){
+      console.log("4")
+      return true}
+
+    if(values[0][1] != undefined && values[0][1] == values[1][1]!= undefined && values[1][1] && values[1][1] == values[2][1] && values[2][1] != undefined){
+      console.log("5")
+      return true}
+ 
+    if(values[0][2] != undefined && values[0][2] == values[1][2]!= undefined && values[1][2] && values[1][2] == values[2][2] && values[2][2] != undefined){
+      console.log("6")
+      return true}
+ 
+    if(values[0][0] != undefined && values[0][0] == values[1][1]!= undefined && values[1][1] && values[1][1] == values[2][2] && values[2][2] != undefined){
+      console.log("7")
+      return true}
+
+    if(values[0][2] != undefined && values[0][2] == values[1][1]!= undefined && values[1][1] && values[1][1] == values[2][0] && values[2][0] != undefined){
+      console.log("8")
+      return true}
+
+    return false
+  }
+
+
   return (
     <main className={styles.main}>
-      
+      {winner && <div>
+        {winner} Ganhou!
+      </div>}
       <div>
 
         <div className={styles.boxRow}>
