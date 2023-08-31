7273384aa94904c37b9d1c6655aa34159ae3b58e
Pedro Sanches <sanchespedro40@gmail.com>
2023-08-31 17:05:23 -0300

    inicializating-CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,8 +56,6 @@ export default function Home() {
 
     return false
   }
-
-
   return (
     <main className={styles.main}>
       {winner && <div>
@@ -70,20 +68,23 @@ export default function Home() {
             <button onClick={() => setvalue(0,0)} className={styles.box}>{values[0][0]}</button>
             <button onClick={() => setvalue(0,1)} className={styles.box}>{values[0][1]}</button>
             <button onClick={() => setvalue(0,2)} className={styles.box}>{values[0][2]}</button>
-            
+
         </div>
 
         <div className={styles.boxRow}>
             <button onClick={() => setvalue(1,0)} className={styles.box}>{values[1][0]}</button>
             <button onClick={() => setvalue(1,1)} className={styles.box}>{values[1][1]}</button>
             <button onClick={() => setvalue(1,2)} className={styles.box}>{values[1][2]}</button>
+
         </div>
         
         <div className={styles.boxRow}>
             <button onClick={() => setvalue(2,0)} className={styles.box}>{values[2][0]}</button>
             <button onClick={() => setvalue(2,1)} className={styles.box}>{values[2][1]}</button>
             <button onClick={() => setvalue(2,2)} className={styles.box}>{values[2][2]}</button>
+
         </div>
+
 
       </div>
 
